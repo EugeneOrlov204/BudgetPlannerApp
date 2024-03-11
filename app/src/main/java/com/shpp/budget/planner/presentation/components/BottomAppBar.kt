c7a6d9307777c89e468059259e3ee608c4c63d8c
Anastasiia <anastasiiahrokholska@gmail.com>
2024-03-11 19:25:12 +0200

    86bxpgy2t: reworked bottom app bar component, added enum for BottomAppBar elements

--- app/src/main/java/com/shpp/budget/planner/presentation/components/BottomAppBar.kt
+++ app/src/main/java/com/shpp/budget/planner/presentation/components/BottomAppBar.kt
@@ -2,25 +2,22 @@ package com.shpp.budget.planner.presentation.components
 
 import androidx.annotation.FloatRange
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.CalendarToday
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.filled.Wallet
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
@@ -39,26 +36,36 @@ import com.shpp.budget.planner.R
 import com.shpp.budget.planner.presentation.theme.BudgetPlannerAppTheme
 import com.shpp.budget.planner.presentation.utils.ext.toPath
 
+/**
+ * Draw height is extended on top by
+ *
+ * [barPaddingVertical]+([plusIconSize] + [plusPadding] * 2)*[plusButtonSpacingPercent]
+ */
 @Composable
 fun BottomAppBar(
+    currentScreen: BottomBarScreen,
     plusButtonColor: Color = MaterialTheme.colorScheme.secondary,
     plusIconColor: Color = MaterialTheme.colorScheme.onSecondary,
     plusPadding: Dp = dimensionResource(R.dimen.home_screen_central_button_padding),
     plusIconSize: Dp = dimensionResource(R.dimen.home_screen_central_button_icon_size),
     barPaddingVertical: Dp = dimensionResource(R.dimen.home_screen_bottom_bar_padding_vertical),
     barColor: Color = MaterialTheme.colorScheme.tertiary,
-    @FloatRange(from = 0.0, to = 1.0) plusButtonSpacingPercent: Float = 0.2f
+    @FloatRange(from = 0.0, to = 1.0) plusButtonSpacingPercent: Float = 0.2f,
+    onPlusClick: () -> Unit = {},
+    onScreenClick: (screen: BottomBarScreen) -> Unit = {}
 ) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+    Box(contentAlignment = Alignment.Center) {
         val density = LocalDensity.current
-        val plusSizePx by remember {
-            derivedStateOf { with(density) { (plusIconSize + plusPadding * 2).toPx() } }
-        }
+        val plusSize by remember { derivedStateOf { (plusIconSize + plusPadding * 2) } }
+        val plusSizePx by remember { derivedStateOf { with(density) { plusSize.toPx() } } }
         val barPaddingVerticalPx by remember {
             derivedStateOf { with(density) { barPaddingVertical.toPx() } }
         }
+
         Icon(
             modifier = Modifier
+                .offset(y = -(plusSize * (1 + plusButtonSpacingPercent)))
+                .clickable { onPlusClick() }
                 .background(color = plusButtonColor, shape = CircleShape)
                 .padding(plusPadding)
                 .size(plusIconSize),
@@ -74,11 +81,11 @@ fun BottomAppBar(
                     val holeSize = plusSizePx + clearance
                     val polygon = RoundedPolygon(
                         vertices = floatArrayOf(
-                            0f, -barPaddingVerticalPx,
-                            (size.width - 1.5f * holeSize) / 2, -barPaddingVerticalPx,
+                            0f, -barPaddingVerticalPx - clearance,
+                            (size.width - 1.5f * holeSize) / 2, -barPaddingVerticalPx - clearance,
                             size.width / 2, clearance * 2f,
-                            (size.width + 1.5f * holeSize) / 2, -barPaddingVerticalPx,
-                            size.width, -barPaddingVerticalPx,
+                            (size.width + 1.5f * holeSize) / 2, -barPaddingVerticalPx - clearance,
+                            size.width, -barPaddingVerticalPx - clearance,
                             size.width, size.height,
                             0f, size.height
                         ),
@@ -97,56 +104,29 @@ fun BottomAppBar(
                         drawPath(path, color = barColor)
                     }
                 }
-                .padding(bottom = barPaddingVertical),
+                .padding(bottom = (barPaddingVertical - plusSize * plusButtonSpacingPercent * 2))
+                .offset(y = -plusSize * plusButtonSpacingPercent * 2),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Box(
-                modifier = Modifier.weight(1f),
-                contentAlignment = Alignment.Center
-            ) {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Filled.Home,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onTertiary
-                    )
+            BottomBarScreen.entries.forEachIndexed { index, bottomBarScreen ->
+                Box(
+                    modifier = Modifier.weight(1f),
+                    contentAlignment = Alignment.Center
+                ) {
+                    IconButton(onClick = { onScreenClick(bottomBarScreen) }) {
+                        Icon(
+                            imageVector = bottomBarScreen.icon,
+                            contentDescription = null,
+                            tint = if (currentScreen == bottomBarScreen) {
+                                MaterialTheme.colorScheme.onSecondary
+                            } else {
+                                MaterialTheme.colorScheme.onTertiary
+                            }
+                        )
+                    }
                 }
-            }
-            Box(
-                modifier = Modifier.weight(1f),
-                contentAlignment = Alignment.Center
-            ) {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Filled.CalendarToday,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onTertiary
-                    )
-                }
-            }
-            Spacer(modifier = Modifier.weight(1f))
-            Box(
-                modifier = Modifier.weight(1f),
-                contentAlignment = Alignment.Center
-            ) {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Filled.Wallet,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onTertiary
-                    )
-                }
-            }
-            Box(
-                modifier = Modifier.weight(1f),
-                contentAlignment = Alignment.Center
-            ) {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Filled.Person,
-                        contentDescription = null,
-                        tint = MaterialTheme.colorScheme.onTertiary
-                    )
+                if (index == (BottomBarScreen.entries.size / 2) - 1) {
+                    Spacer(modifier = Modifier.weight(1f))
                 }
             }
         }
@@ -157,8 +137,13 @@ fun BottomAppBar(
 @Composable
 private fun BottomAppBarPreview() {
     BudgetPlannerAppTheme {
-        Surface(color = MaterialTheme.colorScheme.background) {
-            BottomAppBar()
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.Red),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            BottomAppBar(currentScreen = BottomBarScreen.HOME)
         }
     }
 }
