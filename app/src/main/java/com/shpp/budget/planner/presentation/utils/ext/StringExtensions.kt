4ce8c833d7274471c3c8e8b32ad4895a8a71fbd5
Yurii <grigoriigedz1703@gmail.com>
2024-04-07 15:35:35 +0200

    86bxzn0jm exported pattern to constant

--- app/src/main/java/com/shpp/budget/planner/presentation/utils/ext/StringExtensions.kt
+++ app/src/main/java/com/shpp/budget/planner/presentation/utils/ext/StringExtensions.kt
@@ -1,6 +1,10 @@
 package com.shpp.budget.planner.presentation.utils.ext
 
+const val MAX_ALLOWED_AMOUNT_OF_NUMBERS = 3
+const val THREE_LAST_NUMBERS_PATTERN = "\\d{$MAX_ALLOWED_AMOUNT_OF_NUMBERS}\$"
 fun String.toReducedNumberFormat(): String {
-    return if (this.length > 3) this.replace(Regex("\\d{3}\$"), "k")
+    return if (this.length > MAX_ALLOWED_AMOUNT_OF_NUMBERS) this.replace(
+        Regex(THREE_LAST_NUMBERS_PATTERN), "k"
+    )
     else this
 }
