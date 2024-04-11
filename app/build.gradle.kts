225994a03b0d7e314b3d856a3ab0276cfe94ed7b
Ivanna <spolskaivanna@gmail.com>
2024-04-11 16:47:26 +0300

    Removed redundant dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.benchmark:benchmark-macro:1.2.3")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
