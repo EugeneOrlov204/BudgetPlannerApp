87e289f670ec057ab215f2863c2d582ba026e535
Ivanna <spolskaivanna@gmail.com>
2024-04-22 09:58:03 +0300

    Added some changes

--- app/src/main/java/com/shpp/budget/planner/presentation/keyboard/Keyboard.kt
+++ app/src/main/java/com/shpp/budget/planner/presentation/keyboard/Keyboard.kt
@@ -23,58 +23,36 @@ import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.sp
 import com.shpp.budget.planner.R
 
+const val textButtonNormalTextSize = 20
+const val textButtonSmallTextSize = 15
+
 @Composable
 fun Keyboard() {
-    val textButtonNormalTextSize = 20
-    val textButtonSmallTextSize = 15
+
     Column(
         modifier = Modifier
             .fillMaxSize(),
         verticalArrangement = Arrangement.Bottom
     ) {
-        ColumnSpacer()
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            CreateFilledButton(text = stringResource(id = R.string.keyboard_button_1))
-            CreateFilledButton(text = stringResource(id = R.string.keyboard_button_2))
-            CreateFilledButton(text = stringResource(id = R.string.keyboard_button_3))
-            CreateTextButton(
-                text = stringResource(id = R.string.keyboard_button_minus),
-                textButtonNormalTextSize
-            )
-        }
-        ColumnSpacer()
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            CreateFilledButton(text = stringResource(id = R.string.keyboard_button_4))
-            CreateFilledButton(text = stringResource(id = R.string.keyboard_button_5))
-            CreateFilledButton(text = stringResource(id = R.string.keyboard_button_6))
-            CreateTextButton(
-                text = stringResource(id = R.string.keyboard_button_plus),
-                textButtonNormalTextSize
-            )
-        }
-        ColumnSpacer()
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            CreateFilledButton(text = stringResource(id = R.string.keyboard_button_7))
-            CreateFilledButton(text = stringResource(id = R.string.keyboard_button_8))
-            CreateFilledButton(text = stringResource(id = R.string.keyboard_button_9))
-            CreateTextButton(
-                text = stringResource(id = R.string.keyboard_button_equals),
-                textButtonNormalTextSize
-            )
-        }
-        ColumnSpacer()
+        CreateKeyboardRow(
+            firstButton = KeyboardButtonsSymbols.BUTTON_1,
+            secondButton = KeyboardButtonsSymbols.BUTTON_2,
+            thirdButton = KeyboardButtonsSymbols.BUTTON_3,
+            fourthButton = KeyboardButtonsSymbols.BUTTON_MINUS
+        )
+        CreateKeyboardRow(
+            firstButton = KeyboardButtonsSymbols.BUTTON_4,
+            secondButton = KeyboardButtonsSymbols.BUTTON_5,
+            thirdButton = KeyboardButtonsSymbols.BUTTON_6,
+            fourthButton = KeyboardButtonsSymbols.BUTTON_PLUS
+        )
+        CreateKeyboardRow(
+            firstButton = KeyboardButtonsSymbols.BUTTON_7,
+            secondButton = KeyboardButtonsSymbols.BUTTON_8,
+            thirdButton = KeyboardButtonsSymbols.BUTTON_9,
+            fourthButton = KeyboardButtonsSymbols.BUTTON_EQUAL
+        )
+        KeyboardColumnSpacer()
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
@@ -83,14 +61,37 @@ fun Keyboard() {
             CreateTextButton(text = " ", textButtonNormalTextSize)
             CreateFilledButton(text = stringResource(id = R.string.keyboard_button_0))
             CreateTextButton(text = stringResource(id = R.string.keyboard_button_x), textButtonNormalTextSize)
-            CreateTextButton(text = stringResource(id = R.string.keyboard_button_done), textButtonSmallTextSize )
+            CreateTextButton(text = stringResource(id = R.string.keyboard_button_done).uppercase(), textButtonSmallTextSize )
         }
-        ColumnSpacer()
+        KeyboardColumnSpacer()
     }
 }
 
 @Composable
-fun CreateFilledButton(text: String) {
+private fun CreateKeyboardRow(
+    firstButton: String,
+    secondButton: String,
+    thirdButton: String,
+    fourthButton: String,
+) {
+    KeyboardColumnSpacer()
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        CreateFilledButton(firstButton)
+        CreateFilledButton(secondButton)
+        CreateFilledButton(thirdButton)
+        CreateTextButton(
+            fourthButton,
+            textButtonNormalTextSize
+        )
+    }
+}
+
+@Composable
+private fun CreateFilledButton(text: String) {
     Button(
         onClick = { /*TODO*/ },
         colors = ButtonColors(Color.White, Color.Black, Color.White, Color.Black),
@@ -107,7 +108,7 @@ fun CreateFilledButton(text: String) {
 }
 
 @Composable
-fun CreateTextButton(text: String, fontSize: Int) {
+private fun CreateTextButton(text: String, fontSize: Int) {
     TextButton(
         onClick = { /*TODO*/ },
         colors = ButtonColors(Color.Transparent, Color.Blue, Color.Transparent, Color.Blue),
@@ -123,7 +124,7 @@ fun CreateTextButton(text: String, fontSize: Int) {
 }
 
 @Composable
-fun ColumnSpacer() {
+private fun KeyboardColumnSpacer() {
     Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.keyboard_rows_space_size)))
 }
 
@@ -132,4 +133,21 @@ fun ColumnSpacer() {
 @Composable
 fun KeyboardPreview() {
     Keyboard()
+}
+
+object KeyboardButtonsSymbols {
+    const val BUTTON_1 = "1"
+    val BUTTON_2 = "2"
+    val BUTTON_3 = "3"
+    val BUTTON_4 = "4"
+    val BUTTON_5 = "5"
+    val BUTTON_6 = "6"
+    val BUTTON_7 = "7"
+    val BUTTON_8 = "8"
+    val BUTTON_9 = "9"
+    val BUTTON_0 = "0"
+    val BUTTON_MINUS = "-"
+    val BUTTON_PLUS = "+"
+    val BUTTON_EQUAL = "="
+    val BUTTON_X = "x"
 }
